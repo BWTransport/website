4bb580707132825d8dccc968b390cd2a72fde588
Jack Browne <jackbrowne00@gmail.com>
2022-04-11 13:56:51 +0100

    add alt to images for accessability

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,9 +14,9 @@ const Navbar = () => {
       <div className="flex items-center h-20 justify-between bg-act-grey pr-4">
         <div className="cursor-pointer flex items-center">
           <Link href="/">
-            <a className="w-40">
-              <Image src={logo} />
-            </a>
+            <div className="w-40">
+              <Image src={logo} alt="logo" />
+            </div>
           </Link>
         </div>
         <ul className="flex items-center">
@@ -26,9 +26,9 @@ const Navbar = () => {
               className="px-2 sm:px-4 text-sm sm:text-base text-center"
             >
               <Link href={link.link}>
-                <a className="text-white hover:text-gray-400 duration-500">
+                <div className="text-white hover:text-gray-400 duration-500">
                   {link.name}
-                </a>
+                </div>
               </Link>
             </li>
           ))}
